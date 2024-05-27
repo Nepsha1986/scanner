0c28787ee3dd6587a995aead0ba0ef10769aa6b6
Alex Nepsha <nepsha1986@gmail.com>
2024-05-27 15:41:33 +0300

    tiny fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		lib: {
-			entry: 'src/main.ts',
+			entry: 'src/Scanner.ts',
 			name: 'Scanner',
 			fileName: (format) => `scanner.${format}.js`,
 			formats: ['umd']
